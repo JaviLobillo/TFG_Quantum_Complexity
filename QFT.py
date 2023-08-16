cc2d124e199b60468f71bf8a74025eda8ee1f111
JaviLobillo <120462700+JaviLobillo@users.noreply.github.com>
2023-08-17 00:56:06 +0200

    Add files via upload

--- QFT.py
+++ QFT.py
@@ -2,7 +2,6 @@ import numpy as np
 from qiskit import *
 from qiskit_ibm_provider import IBMProvider
 from qiskit.visualization import plot_histogram
-import matplotlib.figure as fig
 print("Imports Successful")
 
 provider = IBMProvider()
@@ -34,38 +33,117 @@ def bounded_qubits_qft(n, m):
         circuit.swap(i, n-i-1)
     return circuit
 
-def generic_circuit(n, BE):
+def generic_circuit(n, BE, l):
     h_cir = QuantumCircuit(n)
     for i in range(n):
         h_cir.h(i)
     qft_cir = generic_qft(n)
     g_cir = h_cir.compose(qft_cir)
     g_cir.measure_all()
-    g_cir.draw(output='mpl', reverse_bits=True, filename='drawn_generic_circuit.png')
+    g_cir.draw(output='mpl', reverse_bits=True, filename='pic_generic_q=' + str(n) + '.png')
     backend = provider.get_backend(BE)
     transpiled = transpile(g_cir, backend)
     job = backend.run(transpiled, shots = 8000)
     counts = job.result().get_counts()
-    plot = plot_histogram(counts)
-    plot.savefig('results_generic_circuit_' + BE + '.png')
+    plot = plot_histogram(counts, figsize=(l, 11))
+    plot.savefig('res_generic_' + BE + '_q=' + str(n) + '.png')
     
-def bounded_circuit(n, m, BE):
+def bounded_circuit(n, m, BE, l):
     h_cir = QuantumCircuit(n)
     for i in range(n):
         h_cir.h(i)
     qft_cir = bounded_qubits_qft(n, m)
     b_cir = h_cir.compose(qft_cir)
     b_cir.measure_all()
-    b_cir.draw(output='mpl', reverse_bits=True, filename='drawn_bounded_circuit.png')
+    b_cir.draw(output='mpl', reverse_bits=True, filename='pic_bounded_q=' + str(n) + '_b=' + str(m) + '.png')
     backend = provider.get_backend(BE)
     transpiled = transpile(b_cir, backend)
     job = backend.run(transpiled, shots = 8000)
     counts = job.result().get_counts()
-    plot = plot_histogram(counts)
-    plot.savefig('results_bounded_circuit_' + BE + '.png')
+    plot = plot_histogram(counts, figsize=(l, 11))
+    plot.savefig('res_bounded_' + BE + '_q=' + str(n) + '_b=' + str(m) + '.png')
 
-generic_circuit(5, 'simulator_statevector')
-generic_circuit(5, 'ibmq_manila')
+generic_circuit(3, 'simulator_statevector', 20)
+generic_circuit(3, 'ibm_lagos', 20)
+print(3)
 
-bounded_circuit(5, 2, 'simulator_statevector')
-bounded_circuit(5, 2, 'ibmq_manila')
+bounded_circuit(3, 1, 'simulator_statevector', 20)
+bounded_circuit(3, 1, 'ibm_lagos', 20)
+print(3, 1)
+print("3 qubits done")
+
+generic_circuit(4, 'simulator_statevector', 23)
+generic_circuit(4, 'ibm_lagos', 23)
+print(4)
+
+bounded_circuit(4, 1, 'simulator_statevector', 23)
+bounded_circuit(4, 1, 'ibm_lagos', 23)
+print(4, 1)
+
+bounded_circuit(4, 2, 'simulator_statevector', 23)
+bounded_circuit(4, 2, 'ibm_lagos', 23)
+print(4, 2)
+print("4 qubits done")
+
+generic_circuit(5, 'simulator_statevector', 25)
+generic_circuit(5, 'ibm_lagos', 25)
+print(5)
+
+bounded_circuit(5, 1, 'simulator_statevector', 25)
+bounded_circuit(5, 1, 'ibm_lagos', 25)
+print(5, 1)
+
+bounded_circuit(5, 2, 'simulator_statevector', 25)
+bounded_circuit(5, 2, 'ibm_lagos', 25)
+print(5, 2)
+
+bounded_circuit(5, 3, 'simulator_statevector', 25)
+bounded_circuit(5, 3, 'ibm_lagos', 25)
+print(5, 3)
+print("5 qubits done")
+
+generic_circuit(6, 'simulator_statevector', 28)
+generic_circuit(6, 'ibm_lagos', 28)
+print(6)
+
+bounded_circuit(6, 1, 'simulator_statevector', 28)
+bounded_circuit(6, 1, 'ibm_lagos', 28)
+print(6, 1)
+
+bounded_circuit(6, 2, 'simulator_statevector', 28)
+bounded_circuit(6, 2, 'ibm_lagos', 28)
+print(6, 2)
+
+bounded_circuit(6, 3, 'simulator_statevector', 28)
+bounded_circuit(6, 3, 'ibm_lagos', 28)
+print(6, 3)
+
+bounded_circuit(6, 4, 'simulator_statevector', 28)
+bounded_circuit(6, 4, 'ibm_lagos', 28)
+print(6, 4)
+print("6 qubits done")
+
+generic_circuit(7, 'simulator_statevector', 30)
+generic_circuit(7, 'ibm_lagos', 30)
+print(7)
+
+bounded_circuit(7, 1, 'simulator_statevector', 30)
+bounded_circuit(7, 1, 'ibm_lagos', 30)
+print(7, 1)
+
+bounded_circuit(7, 2, 'simulator_statevector', 30)
+bounded_circuit(7, 2, 'ibm_lagos', 30)
+print(7, 2)
+
+bounded_circuit(7, 3, 'simulator_statevector', 30)
+bounded_circuit(7, 3, 'ibm_lagos', 30)
+print(7, 3)
+
+bounded_circuit(7, 4, 'simulator_statevector', 30)
+bounded_circuit(7, 4, 'ibm_lagos', 30)
+print(7, 4)
+
+bounded_circuit(7, 5, 'simulator_statevector', 30)
+bounded_circuit(7, 5, 'ibm_lagos', 30)
+print(7, 5)
+print("7 qubits done")
